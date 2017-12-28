958fda85db6c4b220a33b50e915b39ba5cb19e3e
Ben Martin <ben_m365@hotmail.co.uk>
2017-12-28 22:43:20 +0000

    stylin' so fine, you can call me Gok

--- js/galleryScripts.js
+++ js/galleryScripts.js
@@ -2,30 +2,30 @@ var slideIndex = 1;
 var imgCapt;
 document.getElementsByTagName("body")[0].addEventListener("load", initGallery(), false);
 
-function initGallery(){
+function initGallery() {
   imgCapt = JSON.parse(imgTxt);
   showSlides(slideIndex);
 }
 
-function plusSlides(n){
+function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function showSlides(n){
+function showSlides(n) {
   var i = imgCapt.imgData.length;
   var imageToChange = document.getElementById("currentImage");
-  if(n-1 >= i){
+  if (n - 1 >= i) {
     n = 1;
   }
-  else if(n == 0){
+  else if (n == 0) {
     n = i;
   }
   slideIndex = n;
   var imageNumber = document.getElementById("numberText");
   var imageNumberText = n + " / " + i;
   imageNumber.innerHTML = imageNumberText;
-  imageToChange.src = imgCapt.imgData[n-1].source;
+  imageToChange.src = imgCapt.imgData[n - 1].source;
   var imageCaption = document.getElementById("caption");
-  imageCaption.innerHTML = imgCapt.imgData[n-1].caption;
-  
+  imageCaption.innerHTML = imgCapt.imgData[n - 1].caption;
+
 }
