3f03c8406e8e6e486cd7e92604acef27994694c2
MajorVengeance <ben_m365@hotmail.co.uk>
2017-12-29 14:33:56 +0000

    Validator Changes
    changes to appease the great w3c validator

--- js/quoteScripts.js
+++ js/quoteScripts.js
@@ -3,13 +3,12 @@ var date = new Date();
 var map;
 var geocoder;
 
-function initPage(){
-  //vanillacalendar.init();
+function initPage() {
   initMap();
 }
 
-function initMap(){
-  var cambridge = {lat: 52.205318, lng: 0.121864};
+function initMap() {
+  var cambridge = { lat: 52.205318, lng: 0.121864 };
   geocoder = new google.maps.Geocoder();
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
@@ -19,7 +18,7 @@ function initMap(){
   loadFromJSON();
 }
 
-function addMarker(latLng){
+function addMarker(latLng) {
   var marker = new google.maps.Marker({
     position: latLng,
     map: map
@@ -27,108 +26,108 @@ function addMarker(latLng){
   markers.push(marker);
 }
 
-function clearMarkers(){
+function clearMarkers() {
   setMapOnAll(null);
 }
 
-function setMapOnAll(map){
-  for(var i=0; i< markers.length; i++){
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
   }
 }
 
-function deleteMarkers(){
+function deleteMarkers() {
   clearMarkers();
   markers = [];
 }
 
-function showMarkers(){
+function showMarkers() {
   setMaponAll(map);
 }
 
-function loadFromJSON(){
+function loadFromJSON() {
   let obj = JSON.parse(mapLatLng);
-  
-  for(i in obj.TestData){
+
+  for (i in obj.TestData) {
     var latLng = new google.maps.LatLng(obj.TestData[i].lat, obj.TestData[i].lng);
     addMarker(latLng);
   }
 }
 
-function codeAddress(address){
-  geocoder.geocode( {'address': address}, function(results, status) {
-    if(status == 'OK'){
-        addMarker(results[0].geometry.location);
-		return true;
-    } else{
+function codeAddress(address) {
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    if (status == 'OK') {
+      addMarker(results[0].geometry.location);
+      return true;
+    } else {
       alert('Geocode was not successful for the following reason: ' + status);
-	  return false;
+      return false;
     }
   });
 }
 
-function makeBooking(){
-	var address = "";
-	var x = document.forms["bookQuote"]["name"].value;
-	if(x == ""){
-		alert("Name must be filled out");
-		return false;
-	}
-	
-	x = document.forms["bookQuote"]["add1"].value;
-	if(x == ""){
-		alert("Address Line 1 must be filled out");
-		return false;
-	}
-	address += x;
-	
-	x = document.forms["bookQuote"]["add2"].value;
-	if(x !== ""){
-		address += ", " + x;
-	}
-	
-	x = document.forms["bookQuote"]["city"].value;
-	if(x == ""){
-		alert("City must be filled out");
-		return false;
-	}
-	address += ", " + x;
-	
-	x = document.forms["bookQuote"]["postcode"].value;
-	if(x == ""){
-		alert("Postcode must be filled out");
-		return false;
-	}
-	else if(!validatePostcode(x)){
-			return false;
-	}
+function makeBooking() {
+  var address = "";
+  var x = document.forms["bookQuote"]["name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+
+  x = document.forms["bookQuote"]["add1"].value;
+  if (x == "") {
+    alert("Address Line 1 must be filled out");
+    return false;
+  }
+  address += x;
+
+  x = document.forms["bookQuote"]["add2"].value;
+  if (x !== "") {
+    address += ", " + x;
+  }
+
+  x = document.forms["bookQuote"]["city"].value;
+  if (x == "") {
+    alert("City must be filled out");
+    return false;
+  }
   address += ", " + x;
-  
+
+  x = document.forms["bookQuote"]["postcode"].value;
+  if (x == "") {
+    alert("Postcode must be filled out");
+    return false;
+  }
+  else if (!validatePostcode(x)) {
+    return false;
+  }
+  address += ", " + x;
+
   x = document.forms["bookQuote"]["contactNo"].value;
-	if(x == ""){
-		alert("Contact Number must be filled out");
-		return false;
-	}
-	else if(!validateContactNo(x)){
-			return false;
-	}
+  if (x == "") {
+    alert("Contact Number must be filled out");
+    return false;
+  }
+  else if (!validateContactNo(x)) {
+    return false;
+  }
   address += ", " + x;
-	
-	if(!codeAddress(address)){
-		return false;
-	}
-	
-	var form = document.getElementsByName('bookQuote');
-	form[0].submit();
+
+  if (!codeAddress(address)) {
+    return false;
+  }
+
+  var form = document.getElementsByName('bookQuote');
+  form[0].submit();
 }
 
-function validateContactNo(number){
+function validateContactNo(number) {
   var regex = /[0-9]+/i;
   return regex.test(number);
 }
 
-function validatePostcode(postcode){
-	postcode = postcode.replace(/\s/g, "");
-	var regex = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
-	return regex.test(postcode);
+function validatePostcode(postcode) {
+  postcode = postcode.replace(/\s/g, "");
+  var regex = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
+  return regex.test(postcode);
 }
